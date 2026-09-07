548887b8acdca8f01edcb29683c7e68e4bfb1999
Karim <oucifkarimo17@gmail.com>
2026-09-07 14:19:50 +0100

    bas

---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="bg-background text-foreground flex justify-center items-center min-h-screen flex-col gap-4">
+        <p>Hello main page</p>
+        <Button>Click here</Button>    
+    </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+import tailwindcss from "@tailwindcss/vite"
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css : ["~/assets/main.css"],
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  modules: ["shadcn-nuxt"],
+
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  },
+
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    }
+  ]
+
+})
