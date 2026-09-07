c8a08cf93cafc1644d9b3398b60d4da9bebc0f52
Karim <oucifkarimo17@gmail.com>
2026-09-07 14:40:43 +0100

    starter-tail-

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="bg-background text-foreground flex justify-center items-center min-h-screen flex-col gap-4">
         <p>Hello main page</p>
-        <Button>Click here</Button>    
+        <!-- <Label for="email">Karim</Label>
+        <Button>Click here</Button>     -->
     </div>
 </template>
